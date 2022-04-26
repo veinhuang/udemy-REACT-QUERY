6acf4d9a3b518e8976d2fad0f7f289bd35f38ab6
Wayne Huang <wayne.huang@loblaw.ca>
2022-04-25 18:02:29 -0700

    76. Testing Rendered Query Data

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -3,6 +3,12 @@ import { screen } from '@testing-library/react';
 import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
+test('renders response from query', async () => {
   renderWithQueryClient(<Treatments />);
+
+  const treatmentTitles = await screen.findAllByRole('heading', {
+    name: /massage|facial|scrub/i,
+  });
+
+  expect(treatmentTitles).toHaveLength(3);
 });
