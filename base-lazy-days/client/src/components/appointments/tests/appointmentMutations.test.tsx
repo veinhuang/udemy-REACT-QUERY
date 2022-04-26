3b1cb92ebf90dea3ead6baeef95621e65fe79860
Wayne Huang <wayne.huang@loblaw.ca>
2022-04-25 22:18:04 -0700

    81. Code Quiz! Test Cancel Appointment Mutation

--- base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
@@ -40,9 +40,26 @@ test('Reserve appointment', async () => {
 });
 
 test('Cancel appointment', async () => {
-  // your test here
-  //
-  // const cancelButtons = await screen.findAllByRole('button', {
-  //   name: /cancel appointment/i,
-  //  });
+  renderWithQueryClient(
+    <MemoryRouter>
+      <Calendar />
+    </MemoryRouter>,
+  );
+
+  // find all the cancel buttons
+  const cancelButtons = await screen.findAllByRole('button', {
+    name: /cancel appointment/i,
+  });
+
+  // click on the first one to cancel
+  fireEvent.click(cancelButtons[0]);
+
+  // check for the toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('cancel');
+
+  // close alert to keep state clean and wait for it to disappear
+  const alertCloseButton = screen.getByRole('button', { name: 'Close' });
+  alertCloseButton.click();
+  await waitForElementToBeRemoved(alertToast);
 });
