1871d695248ec944d33fa3d999fd49068505fe9e
Wayne Huang <wayne.huang@loblaw.ca>
2022-04-25 22:30:33 -0700

    84. Test Appointments Filter

--- base-lazy-days/client/src/test-utils/index.tsx
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -32,12 +32,10 @@ export function renderWithQueryClient(
   );
 }
 
-// import { defaultQueryClientOptions } from '../react-query/queryClient';
-
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
-// export const createQueryClientWrapper = (): React.FC => {
-//   const queryClient = generateTestQueryClient();
-//   return ({ children }) => (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// };
+export const createQueryClientWrapper = (): React.FC => {
+  const queryClient = generateTestQueryClient();
+  return ({ children }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
